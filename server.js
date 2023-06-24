30ea10a6c5a3a75e31cd695b2829a33f837206c1
lmdcma27 <lmdcma27@gmail.com>
2023-06-23 21:19:19 -0600

    Little changes and configuration for deploy on heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+//install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//Serve only the static files the dist directory
+
+app.use(express.static('./dist/fronted2'));
+
+app.get('/*',(req,res)=>res.sendFile('index.html',{root:'dist/angular-heroku/'}),
+);
+
+//Start the app by listening on the default Heroku port 
+app.listen(process.env.PORT || 8080);
