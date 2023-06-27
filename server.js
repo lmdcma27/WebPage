17273b9f4118dbc2101e9b9161f091fa07aed81b
lmdcma27 <lmdcma27@gmail.com>
2023-06-26 22:12:40 -0600

    minor changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.static('./dist/fronted2'));
 
-app.get('/*',(req,res)=>res.sendFile('index.html',{root:'dist/fronted2/src/'}),
+app.get('/*',(req,res)=>res.sendFile('index.html',{root:'dist/fronted2/'}),
 );
 
 //Start the app by listening on the default Heroku port 
